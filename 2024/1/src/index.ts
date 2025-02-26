51668648c5003798b8b0fbfdc106734beade9232
Max Kelly <maxwill.kelly@gmail.com>
2025-02-26 00:37:18 +0000

    feat(main): Advent of Code Day 1 Part 2

--- 2024/1/src/index.ts
+++ 2024/1/src/index.ts
@@ -1,3 +1,10 @@
+// Advent of Code: 2024, Day 1, Part 1
+// Problem: https://adventofcode.com/2024/day/1
+// Solution:
+//   Part 1: Read the input file, split the lines into two lists, sort the lists,
+//          calculate the distance between the elements of the lists, and sum the distances.
+//   Part 2: Calculate the similarity between the elements of the lists and sum the similarities.
+
 import * as fs from "fs";
 
 const textFile = fs.readFileSync("./input/input.txt", "utf8");
@@ -39,4 +46,17 @@ const totalDistance = distances.reduce((acc, distance) => {
   return acc + distance;
 }, 0);
 
-console.log(totalDistance);
+// Output: 2196996
+console.log("Distance:", totalDistance);
+
+const similarities = lists.left.map((left, index) => {
+  const appearsInRight = lists.right.filter((right) => right === left).length;
+  return left * appearsInRight;
+});
+
+const totalSimilarity = similarities.reduce((acc, similarity) => {
+  return acc + similarity;
+}, 0);
+
+// Output: 23655822
+console.log("Similiarity:", totalSimilarity);
